d03cfce513127505dec2688485218a8b70ce2867
szilard28 <71643751+szilard28@users.noreply.github.com>
2021-12-13 12:58:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Írj programot, mely beolvas három egész számot, és kiírja a képernyőre a legkisebbet!
+
+szam1= int(input('Adja meg az egyik számot! '))
+szam2= int(input('Adja meg a masik szamot! '))
+szam3= int(input('Adja meg a harmadik számot! '))
+
+if szam1 < szam2: 
+  print('Az első szám a legkisebb' )
+
+elif szam2 < szam1:
+  print('A második a legkisebb')
+
+else:
+  print('A harmadik a legkisebb')
